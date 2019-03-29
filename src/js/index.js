c1a69e00368624aa5a0db3da8e862c273066559d
Jitender Kumar <jitkumar10@publicisgroupe.net>
2019-03-29 16:47:54 +0530

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,6 @@
-console.log('First Javascript code');
+//import num from './test.js'
+const x= 15;
+
+console.log(`First Javascript code from imported moduklle ${x}`);
+
+console.log('Happy code code');
